e6f8cb8e46b59150bbe2faef8f8ab6832ca3d433
TeoVargo <teo.vargo@gmail.com>
2024-11-29 16:32:05 +0000

    parameter updates and charts

--- my_functions.py
+++ my_functions.py
@@ -5,6 +5,11 @@ import pandas as pd
 import pretty_midi
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import os
+from typing import Optional
+import seaborn as sns
+
 
 
 # Function to extract notes from the wjazzd.db and do a join of melody and solo_info
@@ -24,7 +29,11 @@ def extract_notes() -> pd.DataFrame :
                            melody.onset as start,
                            melody.onset + melody.duration as end,
                            melody.duration, 
-                           solo_info.instrument
+                           solo_info.instrument,
+                           solo_info.key,
+                           solo_info.style,
+                           solo_info.avgtempo as tempo,
+                           solo_info.rhythmfeel as feel
                         FROM melody
                         JOIN solo_info
                         ON melody.melid = solo_info.melid
@@ -35,13 +44,13 @@ def extract_notes() -> pd.DataFrame :
 # Generate a contour of intervals
 def contour(interval: int):
   if 12 >= interval > 4:
-    return 10
+    return 8
   elif interval > 12:
-    return 20
+    return 16
   elif -12 <= interval < -4:
-    return -10
+    return -8
   elif interval < -12:
-    return -20
+    return -16
   else:
     return interval
 
@@ -131,3 +140,31 @@ def midi_to_notes(midi_file: str):
     prev_start = start
 
   return pd.DataFrame({name: np.array(value) for name, value in notes.items()})
+
+def plot_piano_roll(notes: pd.DataFrame, count: Optional[int] = None):
+  if count:
+    title = f'First {count} notes'
+  else:
+    title = f'Whole track'
+    count = len(notes['pitch'])
+  plt.figure(figsize=(20, 4))
+  plot_pitch = np.stack([notes['pitch'], notes['pitch']], axis=0)
+  plot_start_stop = np.stack([notes['start'], notes['end']], axis=0)
+  plt.plot(
+      plot_start_stop[:, :count], plot_pitch[:, :count], color="b", marker=".")
+  plt.xlabel('Time [s]')
+  plt.ylabel('Pitch')
+  _ = plt.title(title)
+
+def plot_distributions(notes: pd.DataFrame, drop_percentile=2.5):
+  plt.figure(figsize=[15, 5])
+  plt.subplot(1, 3, 1)
+  sns.histplot(notes, x="pitch", bins=20)
+
+  plt.subplot(1, 3, 2)
+  max_step = np.percentile(notes['step'], 100 - drop_percentile)
+  sns.histplot(notes, x="step", bins=np.linspace(0, max_step, 21))
+  
+  plt.subplot(1, 3, 3)
+  max_duration = np.percentile(notes['duration'], 100 - drop_percentile)
+  sns.histplot(notes, x="duration", bins=np.linspace(0, max_duration, 21))
